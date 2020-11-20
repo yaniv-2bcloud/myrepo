74da660f09d5e59e4df23673f44e3c9c1239ed96
DB Lee <donlee@microsoft.com>
2020-11-20 13:36:08 +0900

    update configurations

--- Application_Backend_Deployment/Deploy1.bat
+++ Application_Backend_Deployment/Deploy1.bat
@@ -4,10 +4,22 @@
 Powershell.exe -executionpolicy remotesigned -File ^
     Deploy1.ps1 ^
     --location "westus2" ^
-    --subscriptionID "{put your subscriptionID for deployment}" ^
-    --CertificateEamil "{put you real email address}" ^
-    --existingresourceGroupName "{put your resource group name}" ^
-    --existingstorageAccountName "{put your storage account name}" ^
-    --existingcosmosDBName "{put your CosmosDB Name}" ^
-    --recommendMLServiceURL "{put recommendation ml service endpoint URL from MLStudio}" ^
-    --recommendMLServiceBearerToken "{put service BearerToken from MLStudio}"
+    --subscriptionID "584b6f20-2dee-4f11-a030-af9877a27492" ^
+    --CertificateEamil "dongbum@outlook.com" ^
+    --existingresourceGroupName "contosoretail" ^
+    --existingstorageAccountName "contosoretailstorage" ^
+    --existingcosmosDBName "contosocosmos-sql" ^
+    --recommendMLServiceURL "http://20.190.8.243:80/api/v1/service/retail-ai-item-recommenderv4/score" ^
+    --recommendMLServiceBearerToken "qUIwse3yarzaXNSoeXap0t5LQCGSAJIk"
+
+
+
+
+    @REM --location "westus2" ^
+    @REM --subscriptionID "{put your subscriptionID for deployment}" ^
+    @REM --CertificateEamil "{put you real email address}" ^
+    @REM --existingresourceGroupName "{put your resource group name}" ^
+    @REM --existingstorageAccountName "{put your storage account name}" ^
+    @REM --existingcosmosDBName "{put your CosmosDB Name}" ^
+    @REM --recommendMLServiceURL "{put recommendation ml service endpoint URL from MLStudio}" ^
+    @REM --recommendMLServiceBearerToken "{put service BearerToken from MLStudio}"
